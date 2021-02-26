393374891ec2bc91ba17e3d7dd1d7a56494d3899
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-02-26 15:31:48 +0100

    inserito calcolo prezzo corretto anche in html

--- js/script.js
+++ js/script.js
@@ -6,12 +6,13 @@ var eta = prompt('Quanti anni hai?');
 var prezzo = ( km * 0.21 + " $" );
 //Condizione sconto over 65
 if ( eta >= '65') {
- var prezzo = (( km * 0.21 ) * 40 / 100 + " $");
+ var prezzo = (( km * 0.21 ) -  ( 40 / 100 ) + " $");
 }
 //Condizione sconto under 18
 if ( eta < '18') {
-var prezzo = (( km * 0.21 ) * 20 / 100 + " $");
+var prezzo = (( km * 0.21 ) - ( 20 / 100 )  + " $");
 }
 
 //Stampa Prezzo
 console.log( prezzo );
+document.getElementById('prezzobiglietto').innerHTML = prezzo ;
