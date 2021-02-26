23220dd3291dfa474b6c73fc348f52c030117f38
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-02-26 15:21:37 +0100

    inserito calcolo prezzo in console

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 //Chiedere il numero di km che si vogliono percorrere
-
+var km = prompt('Quanti km devi percorrere?');
 //Chiedere l'età al passegero
+var eta = prompt('Quanti anni hai?');
+//Prezzo di base
+var prezzo = ( km * 0.21 + " $" );
+//Condizione sconto over 65
+if ( eta >= '65') {
+ var prezzo = (( km * 0.21 ) * 40 / 100 + " $");
+}
+//Condizione sconto under 18
+if ( eta < '18') {
+var prezzo = (( km * 0.21 ) * 20 / 100 + " $");
+}
+
+//Stampa Prezzo
+console.log( prezzo );
